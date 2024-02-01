5133ec453382d698a2bed4285927ffde706fde60
Jeudi Prando <jeudi.prando@eletromidia.com.br>
2024-02-01 20:23:26 -0300

    refactor(log): 🔊 pequenos ajustes nos logs

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,34 @@
+import debug from "debug";
 import { UsuarioService } from "./services/usuario";
-import { logErro, logFim, logFimErro, logInicio } from "./utils/logs";
 
 const email = "desenvolvedorads@eletromidia.com.br";
+const log = debug("app:info");
+const logErro = debug("app:erro");
 
 export class App {
   constructor(private readonly usuarios: UsuarioService) {
-    logInicio(new Date());
+    log(new Date(), "inicio");
   }
 
   async start() {
     try {
-      const usuario = await this.usuarios.obterPorEmail(email);
-      if (!usuario) {
-        throw new Error("Usuario nao encontrado!");
-      }
-      console.info("\nUsuario:", usuario, "\n");
-      logFim(new Date());
+      await this.exibirUsuario(email);
     } catch (e) {
       const erro = e instanceof Error ? e : undefined;
       logErro(
         `${erro?.name || ""} ${erro?.message || "Erro inesperado ocorreu"}`,
       );
-      logFimErro(new Date());
     }
+    finally {
+    log( new Date(), "fim");
+    }
+  }
+
+  async exibirUsuario(email: string) {
+    const usuario = await this.usuarios.obterPorEmail(email);
+    if (!usuario) {
+      throw new Error("Usuario nao encontrado!");
+    }
+    console.info("\nUsuario:", usuario, "\n");
   }
 }
--- src/utils/logs.ts
+++
@@ -1,6 +0,0 @@
-import debug from "debug";
-
-export const logInicio = debug("app:inicio");
-export const logFim = debug("app:fim");
-export const logFimErro = debug("app:fim:erro");
-export const logErro = debug("app:erro");
